a6d8b206da37624079abe61aa99ada5a23924cba
tsume <50492756+tsume-ha@users.noreply.github.com>
2020-08-31 23:52:51 +0900

    ready

---
+++ src/components/Calendar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="row">
+        calendar
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Calendar'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <Calendar />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Calendar from '@/components/Calendar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Calendar
   }
 }
 </script>
